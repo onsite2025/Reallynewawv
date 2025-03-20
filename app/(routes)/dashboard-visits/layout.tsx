fa2110aaac85b4de10d46f6959db092c53f72197
onsite2025 <onsitepcn@gmail.com>
2025-03-19 18:06:09 -0700

    Update layout.tsx

--- app/(routes)/dashboard-visits/layout.tsx
+++ app/(routes)/dashboard-visits/layout.tsx
@@ -1,6 +1,8 @@
+// For all layout files (app/(routes)/*/layout.tsx)
 'use client';
 
 import React from 'react';
+// Try direct import from the root of the app
 import AuthGuard from '../../../components/AuthGuard';
 
 export default function DashboardVisitsLayout({
