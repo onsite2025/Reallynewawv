15a679996a8537849fc94bbc01e144b7d19b6cd6
onsite2025 <onsitepcn@gmail.com>
2025-03-19 18:45:18 -0700

    Update layout.tsx

--- app/(routes)/visit-api-test/layout.tsx
+++ app/(routes)/visit-api-test/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import AuthGuard from '../../../components/AuthGuard';
+import AuthGuard from '../../components/AuthGuard';
 
 export default function VisitApiTestLayout({
   children,
